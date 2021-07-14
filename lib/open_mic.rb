7040d4c207c5754b8951bc67d3804933a49bffe2
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-14 16:34:22 -0400

    Add test for performers

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,10 +1,11 @@
 class OpenMic
 
-  attr_reader :location, :date
+  attr_reader :location, :date, :performers
 
   def initialize(location:, date:)
     @location = location
     @date = date
+    @performers = []
   end
 
 end
--- spec/open_mic_spec.rb
+++ spec/open_mic_spec.rb
@@ -20,4 +20,10 @@ RSpec.describe "OpenMic" do
 
     expect(open_mic.date).to eq("11-20-18")
   end
+
+  it "has a location" do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+    expect(open_mic.performers).to eq([])
+  end
 end
