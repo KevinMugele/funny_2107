c90cc2ffeedb78acc271e8901812af13605f35d6
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-14 16:32:40 -0400

    Make tests for class, name, date and initialize variables

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,3 +1,10 @@
 class OpenMic
 
+  attr_reader :location, :date
+
+  def initialize(location:, date:)
+    @location = location
+    @date = date
+  end
+
 end
--- spec/open_mic_spec.rb
+++ spec/open_mic_spec.rb
@@ -3,5 +3,21 @@ require './lib/user'
 require './lib/joke'
 
 RSpec.describe "OpenMic" do
+  it "exists" do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
 
-end 
+    expect(open_mic).to be_a(OpenMic)
+  end
+
+  it "has a location" do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+    expect(open_mic.location).to eq("Comedy Works")
+  end
+
+  it "has a date" do
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+
+    expect(open_mic.date).to eq("11-20-18")
+  end
+end
