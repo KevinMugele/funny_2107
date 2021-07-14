80aa4f880d1e66678a03d07709a12e8b7f7593d8
Kevin Mugele <kevin.mugele@gmail.com>
2021-07-14 17:46:49 -0400

    Complete all methods and tests for iteration 4

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -8,4 +8,14 @@ class OpenMic
     @performers = []
   end
 
+  def welcome(user)
+    @performers << user
+  end
+
+  def repeated_jokes?
+    total_jokes = @performers.map do |performer|
+      performer.jokes
+    end.flatten #To be honest, the reason I got this(.flatten) was because I saw it before Mike told us to delete the files. Did research and understand why I had to use it here.
+    total_jokes.length != total_jokes.uniq.length
+  end
 end
